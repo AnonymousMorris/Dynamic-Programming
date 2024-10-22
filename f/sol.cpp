80d6007fe75c173bcc8ca3eb8ff48c8d2fc1101f
AnonymousMorris <88790211+AnonymousMorris@users.noreply.github.com>
2024-10-22 13:17:21 -0400

    solved lcs

--- f/sol.cpp
+++ f/sol.cpp
@@ -13,37 +13,34 @@ int main() {
 
     int dp[m][n];
 
-    for (int i = 0; i < n; i++) {
+    dp[0][0] = (s[0] == t[0]) ? 1 : 0;
+    // first column of dp
+    for (int i = 1; i < n; i++) {
         dp[0][i] = (s[0] == t[i]) ? 1 : 0;
-        if (i != 0) {
-            dp[0][i] = max(dp[0][i-1], dp[0][i]);
-        }
+        dp[0][i] = max(dp[0][i-1], dp[0][i]);
     }
-
+    // first row of dp
     for (int i = 1; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+        dp[i][0] = (s[i] == t[0]) ? 1 : 0;
+        dp[i][0] = max(dp[i][0], dp[i-1][0]);
+    }
+    // rest of dp
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
             bool match = s[i] == t[j];
-            dp[i][j] = dp[i-1][j];
-            if (j != 0) {
-                dp[i][j] = max(dp[i-1][j-1] + match, dp[i][j]);
-                dp[i][j] = max(dp[i][j-1], dp[i][j]);
-            }
+            dp[i][j] = max({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]+match});
         }
     }
 
-    // for (int i = 0; i < m; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
-
+    // back tracking
     string ans = "";
-    int i, j, tmp;
+    int i, j;
     i = m - 1;
     j = n - 1;
-    tmp = dp[m-1][n-1];
     while (i >=0 && j >= 0) {
+        if (dp[i][j] == 0) {
+            break;
+        }
         if (j > 0 && dp[i][j-1] == dp[i][j]) {
             j--;
             continue;
@@ -53,31 +50,10 @@ int main() {
             continue;
         }
 
-
-        if (j > 0) {
-            if (dp[i][j-1] == dp[i][j]) {
-                j--;
-            }
-            else {
-                if (i > 0) {
-                    if (dp[i-1][j] == dp[i][j]) {
-                        i--;
-                        continue;
-                    }
-                }
-                ans += t[j];
-                i--;
-                j--;
-            }
-        }
-        else {
-            if (dp[i][j] == 1) {
-                ans += t[j];
-            }
-            j--;
-        }
+        ans += t[j];
+        j--;
+        i--;
     }
     reverse(ans.begin(), ans.end());
     cout << ans;
-    // cout << dp[m-1][n-1];
 }
